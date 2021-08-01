85db003e3b62118d538164feecbe6b3490576a20
Michael Rooney <mrooney.github@rowk.com>
2021-08-01 14:24:03 -0500

    handle guess submissions and abstract/clean the response code

--- bot.py
+++ bot.py
@@ -18,17 +18,23 @@ class Game:
         self.rounds = []
         self.players = []
         self.round = 0
+        self.channel = None
+
+    def initialize(self, channel):
+        game.state = 'gathering'
+        game.channel = channel
+        return ["Alright, who's in cowfolks? Can I get an 'In!'?"]
 
     def instate(self, state):
         return self.state == state
 
     def add_player(self, author):
-        player = author.name
+        player = author
         if player not in self.players:
             self.players.append(player)
+        return [':🐴']
 
     def start_game(self):
-        self.state = 'playing'
         return self.next_round()
 
     def next_round(self):
@@ -38,11 +44,30 @@ class Game:
             msgs.append('Alright, starting a game with players: {}'.format(' '.join(self.players)))
         turn = self.round % len(self.players)
         msgs.append("You're up, {}. Slide a truth bomb into my DMs! Everyone else, a fib.".format(self.players[turn]))
+        self.state = 'awaiting_submissions'
+        self.rounds.append({})
         return msgs
 
     def end_round(self):
         self.round += 1
 
+    def submission(self, author, text):
+        #print(repr(author), self.players, author in self.players)
+        if author not in self.players:
+            return [':🚫']
+        round = self.rounds[-1]
+        round[author] = text
+        num_submissions = len(round.keys())
+        msgs = [':✅']
+        if num_submissions == len(self.players):
+            msgs += self.show_submissions()
+        return msgs
+
+    def show_submissions(self):
+        msgs = []
+        self.state = 'awaiting_guesses'
+        msgs.append('Here are the submissions!')
+        return msgs
 
     def __str__(self):
         return '{}: {}'.format(self.state, self.players)
@@ -64,19 +89,28 @@ async def on_message(message):
     direct = str(message.channel.type) == 'private' or client.user.mentioned_in(message)
     command = command_from_message(message.content)
     icommand = lower(command)
+    player = message.author.name
     print('command (direct? {}): {}'.format(direct, icommand))
 
-    if direct and icommand == 'giddy up':
-        await message.channel.send("Alright, who's in cowfolks? Can I get an 'In!'?")
-        game.state = 'gathering'
-    elif game.instate('gathering') and icommand == 'in':
-        game.add_player(message.author)
-        await message.add_reaction('🐴')
-    elif direct and game.instate('gathering') and icommand == 'ready':
-        for msg in game.start_game():
-            await message.channel.send(msg)
+    async def handle_response(response):
+        if response is not None:
+            for msg in response:
+                if len(msg) == 2 and msg[0] == ':':
+                    await message.add_reaction(msg[1])
+                else:
+                    await game.channel.send(msg)
 
-    #TODO: handle DMs of submissions: truth or fibs depending on turn
+    if direct and icommand == 'giddy up':
+        await handle_response(game.initialize(message.channel))
+    elif game.instate('gathering') and icommand == 'in':
+        await handle_response(game.add_player(player))
+    elif direct and game.instate('gathering') and icommand == 'ready':
+        await handle_response(game.start_game())
+    elif direct and game.instate('awaiting_submissions'):
+        await handle_response(game.submission(player, icommand))
+    elif direct and game.instate('awaiting_guesses'):
+        game.guess(player, icommand)
+
     #TODO: when we have a submission from all players, shuffle and display
     #TODO: accept guesses as DMs or spoiler mentions
     #TODO: when we have a guess from players - 1, show the results!
